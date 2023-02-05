d8ef906c4a1a5fa1a1d792621508501419d75f16
Shantanukumar <shantanudive812@gmail.com>
2023-02-05 22:12:07 +0530

    SpringBootProfile Demo Commit

---
+++ src/main/java/com/springprofile/SpringBootProfileDemo/SpringBootProfileDemoApplication.java
@@ -0,0 +1,13 @@
+package com.springprofile.SpringBootProfileDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProfileDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProfileDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springprofile/SpringBootProfileDemo/SpringBootProfileDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springprofile.SpringBootProfileDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProfileDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
